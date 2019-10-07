c07fd0f3c387fe3cf1ae340595360d09081639ba
vsts <vsts@10.1.0.4>
2019-10-07 19:36:53 +0000

    Releasing new version 3.1.0-dev.9-WIFI [skip ci]

--- include/config/MasterConfig.hpp
+++ include/config/MasterConfig.hpp
@@ -4,7 +4,7 @@
 // -- Basic ---------------------------------------
 
 #define FIRMWARE_NAME       "Aetas RoomHub"
-#define FIRMWARE_VERSION    "3.1.0-dev.8-WIFI"
+#define FIRMWARE_VERSION    "3.1.0-dev.9-WIFI"
 
 
 // -- Network --------------------------------------
